ecf28b7a5a78f6215f74dcc14abc68b74cf956d2
Teutorigos <popint94@yahoo.com>
2025-07-15 05:51:32 -0400

    promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,37 +38,61 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    myPromise.then(() => {
+        res.send(JSON.stringify(books,null,4));
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    myPromise.then(() => {
+        const isbn = req.params.isbn;
+        res.send(books[isbn]);
+    })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  var booksList = [];
-  const author = req.params.author;
-  Object.keys(books).forEach(book => {
-    if (books[book].author === author){
-        booksList.push(books[book]);
-    }
-  });
-  res.send(JSON.stringify(booksList,null,4));
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    myPromise.then(() => {
+        var booksList = [];
+        const author = req.params.author;
+        Object.keys(books).forEach(book => {
+            if (books[book].author === author){
+                booksList.push(books[book]);
+            }
+        });
+        res.send(JSON.stringify(booksList,null,4));
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  var booksList = [];
-  const title = req.params.title;
-  Object.keys(books).forEach(book => {
-    if (books[book].title === title){
-        booksList.push(books[book]);
-    }
-  });
-  res.send(JSON.stringify(booksList,null,4));
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    myPromise.then(() => {
+        var booksList = [];
+        const title = req.params.title;
+        Object.keys(books).forEach(book => {
+            if (books[book].title === title){
+                booksList.push(books[book]);
+            }
+        });
+        res.send(JSON.stringify(booksList,null,4));
+    })
 });
 
 //  Get book review
